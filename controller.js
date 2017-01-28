68822d2cc2de4ddf79deb61ff240d4688da5710f
Rory Smith <roarsmith3@gmail.com>
2017-01-28 12:35:25 +0000

    Revert "Added toggling."
    This reverts commit c6a950c0b6ee1fa875fef068f5b9bb900beda7a1.

--- controller.js
+++ controller.js
@@ -1,45 +1,33 @@
 (() => {
   'use strict';
 
-  let toggling_adv;
-
-  let toggleAdv = (originalAdv) => {
-    // turn on
-    console.log("toggling on");
-    eddystone.registerAdvertisement(originalAdv)
-          .then(adv => {
-            // wait 1 sec
-            setTimeout(function(){
-              // turn off
-              console.log("toggling off");
-              adv.unregisterAdvertisement()
-              .then(adv => {
-                // call toggleAdv iff adv == toggling_adv
-                if (originalAdv == toggling_adv) {
-                  console.log("re-toggling");
-                  toggleAdv(originalAdv);
-                } else {
-                  console.log("not re-toggling");
-                }
-              }).catch(error => console.log(error));
-            }, 5 * 1000);
-          }).catch(error => console.log(error));
-  };
-
+  let current_adv;
   let updateCurrentAdvertisement = (id) => {
-      if (id.length === 0 && current_adv) {
-        // disable all
-        // just don't toggle any current ones back on
-        toggling_adv = undefined;
-      } else {
-        let new_adv = {
-          type: 'url',
-          url: ('jeeves://'+id),
-          advertisedTxPower: -100
-        };
-        toggling_adv = new_adv;
-        toggleAdv(toggling_adv);
+    if (id.length == 0 && current_adv) {
+      console.log('unregistering');
+      current_adv.unregisterAdvertisement().then(() => {
+        console.log('Advertisement unregistered successfully.');
+      }).catch(error => console.log(error.message));
+      current_adv = undefined;
+    } else {
+      console.log('updating id to', id);
+      let new_adv = {
+        type: 'url',
+        url: ('jeeves://'+id),
+        advertisedTxPower: -100
+      };
+      if (eddystone.advertisements.length === 0) {
+        eddystone.registerAdvertisement(new_adv)
+          .then(adv => current_adv = adv);
+        return;
       }
+      // unregister existing ads
+      if (current_adv) {
+        current_adv.unregisterAdvertisement()
+          .then(() => eddystone.registerAdvertisement(new_adv))
+          .then(adv => current_adv = adv);
+      }
+    }
   };
 
   let idInput  = document.getElementById("id_input");
